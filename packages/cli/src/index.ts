0a94eec2898f2f6f7ddd0e255c6856326cc6c430
xg-a06 <xg_jser@163.com>
2022-06-09 20:34:40 +0800

    chore: 增加基本的commander流程

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,5 +1,13 @@
-#!/usr/bin/env node
+#! /usr/bin/env node
 import { program } from 'commander';
 import pkg from '../package.json';
 
-console.log(1, pkg.version);
+program
+  .version(pkg.version)
+  .command('serve <name>')
+  .option('-c --config [path]', 'config file path', './avsb.config.js')
+  .action((a, b) => {
+    console.log('remove ' + a, b);
+  });
+
+program.parse(process.argv);
